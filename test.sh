b15a82b9f5f1b477dbcf27a51d876e700702e967
serika1108 <90320143+serika1108@users.noreply.github.com>
2026-03-31 13:06:16 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,5 @@
 python test_video.py --i_frame_model_name ConvChARM \
---i_frame_model_path  checkpoints/ConvChARM.pth --test_config dataset_config_others.json \
---cuda True --output_json_result_path  DCVC_result_psnr.json \
---model_path checkpoints/model_dcvc_quality_0_psnr.pth \
---write_stream False --write_recon_frame False
-
-python test_video.py --i_frame_model_name ConvChARM \
---i_frame_model_path  checkpoints/ConvChARM.pth --test_config dataset_config_HEVC.json \
+--i_frame_model_path  checkpoints/ConvChARM.pth --test_config dataset_config.json \
 --cuda True --output_json_result_path  DCVC_result_psnr.json \
 --model_path checkpoints/model_dcvc_quality_0_psnr.pth \
 --write_stream False --write_recon_frame False
